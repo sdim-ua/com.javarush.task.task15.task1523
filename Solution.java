555504342f4e889b8159928e00a10c2bffe14c8c
sdim-ua <ingersol@yandex.ua>
2019-07-04 15:25:29 +0300

    Create  Solution.java

---
+++ Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.task.task15.task1523;
+
+/* 
+Перегрузка конструкторов
+*/
+
+public class Solution {
+   
+    
+    public Solution () {
+
+    }
+
+    Solution (int i) {
+        
+    }
+
+    protected Solution (double d) {
+        
+    }
+
+    private Solution (String s) {
+       
+    }    
+    
+    public static void main(String[] args) {
+
+    }
+}
+
